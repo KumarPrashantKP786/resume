6048031548e1bff09c20cff4a82d992fc1ddf29f
kpjha786 <kpjha786@gmail.com>
2022-01-03 14:38:44 +0530

    push first

---
+++ src/Components/BodyComponents/AboutUs.jsx
@@ -0,0 +1,81 @@
+import { Box, Container, Grid, Hidden } from "@material-ui/core";
+import React from "react";
+import { useStyles } from "./BodyStyles";
+import image from "../../images/About.jpg";
+import { RenderSectionHeading, CardMedia } from "../common/commonComponent";
+import AcUnitIcon from "@material-ui/icons/AcUnit";
+import PermDeviceInformationIcon from "@material-ui/icons/PermDeviceInformation";
+import ToysIcon from "@material-ui/icons/Toys";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import ScrollAnimation from "react-animate-on-scroll";
+
+export default function AboutUs() {
+  const classes = useStyles();
+
+  const cardMediaData = [
+    {
+      title: " Web Development",
+      description: "",
+      icon: <DashboardIcon />,
+    },
+    {
+      title: "Machine Learning",
+      description: "",
+      icon: <PermDeviceInformationIcon />,
+    },
+    {
+    title: "Graphic Design",
+    description: "",
+    icon: <ToysIcon />,
+  },
+    {
+      title: "Marketing",
+      description: "",
+      icon: <AcUnitIcon />,
+    },
+  ];
+  return (
+    <Box className={classes.section} id='About'>
+      <ScrollAnimation animateIn='fadeIn'>
+        <Container>
+          <Grid container spacing={1}>
+            <Grid item sm={5}>
+              <Box component={Hidden} xsDown>
+                <img
+                  src={image}
+                  alt=' about us'
+                  className={classes.responsiveImg}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              {RenderSectionHeading({
+                smallText: "ABOUT ME",
+                heading: "Hello I'm Kumar Prashant",
+                description:
+                  "A web developer and ML enthusiast who loves to codes something that wiil impact majority of the people in good waay !",
+              })}
+              <br />
+              {/* {CardMedia({
+              label: "labae1",
+              Desc: "Desc1",
+              Icon: <AcUnitIcon />,
+            })} */}
+              <Grid container>
+                {cardMediaData.map((item, i) => (
+                  <Grid item xs={12} sm={6} key={i}>
+                    {CardMedia({
+                      label: item.title,
+                      Desc: item.description,
+                      Icon: item.icon,
+                    })}
+                  </Grid>
+                ))}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Container>
+      </ScrollAnimation>
+    </Box>
+  );
+}
---
+++ src/Components/BodyComponents/Portfolio.jsx
@@ -0,0 +1,72 @@
+import { Box, Button, Container, Grid, Typography } from "@material-ui/core";
+import React from "react";
+import { RenderSectionHeading } from "../common/commonComponent";
+import { useStyles } from "./BodyStyles";
+
+import image1 from "../../images/tictac.png";
+import image2 from "../../images/notepad.jpg";
+import image4 from "../../images/AI.jpg";
+import image3 from "../../images/github.png";
+import image5 from "../../images/5-8.jpg";
+import image6 from "../../images/5-9.jpg";
+import ScrollAnimation from "react-animate-on-scroll";
+
+export default function Portfolio() {
+  const classes = useStyles();
+
+  const portfolioData = [
+    { url: image1, title: " Tic-Tac-Toe ", link: "https://kumarprashantkp786.github.io/TIC-TAC-TOE/" },
+    { url: image2, title: " Todo-List ", link: "https://kumarprashantkp786.github.io/TodoListApp/" },
+    { url: image3, title: " Github profile API ", link: "https://kumarprashantkp786.github.io/github_API_Profile_searcher/" },
+    { url: image4, title: " website php ", link: "https://studyfunduniya.000webhostapp.com/" },
+    { url: image5, title: " Portfolio ", link: "https://studyfunduniya.000webhostapp.com/konnexions2.html" },
+    { url: image6, title: " AI paper ", link: "https://link.springer.com/article/10.1007/s12553-021-00601-2" },
+  ];
+
+  return (
+    <Box className={classes.sectionDark} id='Portfolio'>
+      <ScrollAnimation animateIn='fadeIn'>
+        <Grid
+          container
+          style={{
+            displa: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+          }}>
+          <Grid item xs={12} sm={8}>
+            {RenderSectionHeading({
+              smallText: "Portfolio",
+              heading: "Let's See My Work",
+              alignCenter: true,
+            })}
+          </Grid>
+        </Grid>
+        {/* imge section  */}
+
+        <Container maxWidth='xl'>
+          <Grid container spacing={2}>
+            {portfolioData.map((item, i) => (
+              <Grid item xs={6} sm={6} lg={4} key={i}>
+                <Box className={classes.imageContainer}>
+                  <img
+                    href={item.link}
+                    src={item.url}
+                    alt={item.title}
+                    className={classes.responsiveImg}
+                  />
+                  <Box className={classes.imageOverlay}>
+                    <Typography className={classes.overlayTitle}>
+                      {item.title}
+                    </Typography>
+
+                    <Button variant='contained'><a href={item.link}>Visit</a></Button>
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </ScrollAnimation>
+    </Box>
+  );
+}
