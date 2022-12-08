8d03d382a0de42f0d4a62847293904b6f68d5513
KUMAR PRASHANT <55938012+KumarPrashantKP786@users.noreply.github.com>
2022-12-08 20:56:59 +0530

    Update Theme.jsx

--- src/Components/Theme.jsx
+++ src/Components/Theme.jsx
@@ -2,6 +2,7 @@ export const Theme = {
   colors: {
     base1: "#1e464a", // deep green backgrounf [navbar, about , contact]
     base2: "#fff", //white
+    base3: "124f"
     primary: "#0097a7", //lighter green
     primary1: "#143438", // deepest green [portfolio, footer]
   },
